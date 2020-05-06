81a312b6b141b803942c2fc5eabce6a2c313c447
kumarsumit <sumitkumar.wipro.sk@gmail.com>
2020-05-06 15:35:20 +0100

    first commit

---
+++ testautomation/TestCases/Sampletest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RestSharp;
+using testautomation.RestApiUtilities;
+
+namespace testautomation.TestCases.Sampletest1
+{
+    public class Sampletest1
+    {
+        static void Main(string[] args)
+        {   //sample line of code to call other method 
+            Console.WriteLine("Hi All , This is a sample test case");
+            Console.WriteLine("*******Test case Name - sample test 1 ********");
+            //Console.WriteLine("We are trying to call another method from here");
+            RestUtilities p1 = new RestUtilities();//created object of class here
+            p1.ReturnResponseCode("http://restapi.demoqa.com/utilities/weather/city/", "London");
+            p1.ValidatePresenceOfText("London");
+            p1.ReturnResponseCode("http://restapi.demoqa.com/utilities/weather/city/", "Zurich");
+            p1.ValidatePresenceOfText("London");
+        }
+    }
+}
